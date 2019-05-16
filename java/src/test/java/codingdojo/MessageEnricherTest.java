72ded1c40b18a937ffa51f09ecb6803de64f4a5d
Emily Bache <emily.bache@praqma.com>
2019-05-16 15:25:46 +0200

    reset tests for starting position

--- java/src/test/java/codingdojo/MessageEnricherTest.java
+++ java/src/test/java/codingdojo/MessageEnricherTest.java
@@ -10,64 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MessageEnricherTest {
 
-    private MessageEnricher enricher;
-    private SpreadsheetWorkbook worksheet;
-
-    @BeforeEach
-    void setup() {
-        enricher = new MessageEnricher();
-        worksheet = new SpreadsheetWorkbook();
-    }
-
     @Test
-    void noEnrichmentNeeded() {
+    void sampleTest() {
+        MessageEnricher enricher = new MessageEnricher();
+        SpreadsheetWorkbook worksheet = new SpreadsheetWorkbook();
         Exception e = new RuntimeException("Terrible problem");
-        Approvals.verify(enricher.enrichError(worksheet, e));
+        ErrorResult actual = enricher.enrichError(worksheet, e);
+        assertEquals("Fixme", actual.getMessage());
     }
 
-    @Test
-    void circularReference() {
-        Exception e = new SpreadsheetException("Circular Reference", Arrays.asList("C4", "C5"), null);
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    @Test
-    void noMatchesFound() {
-        Exception e = new SpreadsheetException("No matches found", null, "Missing Token");
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    @Test
-    void circularReferenceWrongException() {
-        Exception e = new RuntimeException("Circular Reference");
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    @Test
-    void noMatchesFoundWrongException() {
-        Exception e = new RuntimeException("No matches found");
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    @Test
-    void expressionParseException() {
-        Exception e = new ExpressionParseException();
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    @Test
-    void objectReferenceNotSet() {
-        Exception e = vLookup();
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
-
-    private Exception vLookup() {
-        return new RuntimeException("Object reference not set to an instance of an object");
-    }
-
-    @Test
-    void objectReferenceNotSetButStacktraceNotVLookup() {
-        Exception e = new RuntimeException("Object reference not set to an instance of an object");
-        Approvals.verify(enricher.enrichError(worksheet, e));
-    }
 }
